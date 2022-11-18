cb67fbfed90dc140ed785d0aa4795e04e070105c
조미곤 <migon99@naver.com>
2022-11-18 17:46:05 +0900

    feat: add loading gif and change css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,64 @@
-import "./App.css";
+import './App.css';
+import React, { useEffect, useState } from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+import FullScreen from './fullscreen.png';
+import Loading from './Loading.gif';
 
-function App() {
-  return <div className="App">Test</div>;
-}
 
-export default App;
+export default function App() {
+  const [LoadingStyle, setLoadingStyle] = useState({ display: 'block' });
+  const [UnityStyle, setUnityStyle] = useState({ display: 'none' });
+
+
+  const { unityProvider, isLoaded, requestFullscreen, loadingProgression } = useUnityContext({
+    loaderUrl: "Build/build.loader.js",
+    dataUrl: "Build/build.data",
+    frameworkUrl: "Build/build.framework.js",
+    codeUrl: "Build/build.wasm",
+  });
+
+  useEffect(() => {
+    if (isLoaded) {
+      let timer = setTimeout(() => {
+        setLoadingStyle({ display: 'none' });
+        setUnityStyle({ display: 'block' });
+      }, 3000);
+      return () => { clearTimeout(timer); }
+    }
+  }, [isLoaded]);
+
+  return (
+    <div>
+      <div style={LoadingStyle}><LoadingComponent loadingProgression={loadingProgression} /></div>
+      <div style={UnityStyle}> <UnityComponent unityProvider={unityProvider} requestFullscreen={requestFullscreen} /></div>
+    </div>
+  );
+};
+
+function LoadingComponent(props) {
+  return (
+    <div className="arrange-center">
+      <img src={Loading} style={{ width: '80vw', height: '45vw', marginBottom: '1vh' }}></img>
+      <div className="bg-gray-600 rounded-full" style={{ width: '50vw' }}>
+        <div className="bg-blue-600 text-xl font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: Math.round(props.loadingProgression * 50) + 'vw' }}>{Math.round(props.loadingProgression * 100)}%</div>
+      </div>
+    </div >
+  );
+};
+
+function UnityComponent(props) {
+  const handleClickEnterFullscreen = () => {
+    props.requestFullscreen(true);
+  }
+  return (
+    <div className="arrange-center">
+      <Unity
+        style={{
+          width: '80vw',
+          height: '45vw',
+        }}
+        unityProvider={props.unityProvider} />
+      <button className="arrange-fullscreen" onClick={handleClickEnterFullscreen}><img src={FullScreen} style={{ padding: '1vw', width: '5vw' }}></img></button>
+    </div>
+  );
+};
