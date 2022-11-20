29cdbc861632f73cb44b6abb6faf4ac1577e2161
마유선 <jasminema@naver.com>
2022-11-20 14:09:24 +0900

    feat: update mongoconfig

--- backend/src/main/java/com/ssafy/thearctic/config/MongoConfig.java
+++ backend/src/main/java/com/ssafy/thearctic/config/MongoConfig.java
@@ -4,20 +4,27 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoDbURI;
+
+    @Value("${spring.data.mongodb.database}")
+    private String mongoDbDatabase;
+
     @Override
     protected String getDatabaseName() {
-        return "theArctic";
+        return mongoDbDatabase;
     }
 
     @Override
     public MongoClient mongoClient() {
-        final ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/theArctic");
+        final ConnectionString connectionString = new ConnectionString(mongoDbURI);
         final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
