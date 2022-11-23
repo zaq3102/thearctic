e58d1c0986da3a7cb1708380f0a687b5fc5c6d70
마유선 <jasminema@naver.com>
2022-11-24 02:34:21 +0900

    feat: update design

--- frontend/src/Components/Layout/Header.js
+++ frontend/src/Components/Layout/Header.js
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../Assets/thearctic_logo.png';
-import '../../Styles/Layout.css';
+import * as React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../Assets/thearctic_logo.png";
+import "../../Styles/Layout.css";
 
 export default function Header() {
   return (
@@ -10,22 +10,50 @@ export default function Header() {
         <div className="container flex items-center justify-between mx-auto">
           <Link to="/" className="flex items-center">
             <img src={logo} className="mr-3 h-10" alt="The Arctic Logo" />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">The Arctic</span>
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              The Arctic
+            </span>
           </Link>
-          <div className="items-center justify-between w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-            <ul className="flex p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text">
+          <div
+            className="items-center justify-between w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
+          >
+            <ul className="flex p-1 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text">
               <li>
-                <Link to="game" className="block py-2 pr-4 pl-3 text-black-700">게임 플레이</Link>
+                <Link
+                  to="game"
+                  className="block py-2 pr-4 pl-3 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400"
+                >
+                  게임 플레이
+                </Link>
               </li>
               <li>
-                <Link to="leaderboard" className="block py-2 pr-4 pl-3 text-gray-700">명예의 전당</Link>
+                <Link
+                  to="leaderboard"
+                  className="block py-2 pr-4 pl-3 text-gray-700"
+                >
+                  명예의 전당
+                </Link>
               </li>
               <li>
-                <Link to="notice" className="block py-2 pr-4 pl-3 text-gray-700">공지사항</Link>
+                <Link
+                  to="notice"
+                  className="block py-2 pr-4 pl-3 text-gray-700"
+                >
+                  공지사항
+                </Link>
               </li>
               <li>
-
-                <a href="https://forms.gle/c6btR3qxQQwZqgMJA" target='_blank' className="feadback block py-2 pr-4 pl-3 text-gray-700">피드백 보내기</a>
+                {/* <a href="https://forms.gle/c6btR3qxQQwZqgMJA" target='_blank' className="feadback block py-2 pr-4 pl-3 text-gray-700">피드백 보내기</a> */}
+                <div className="block py-1 pr-1 pl-1">
+                  <a
+                    href="https://forms.gle/c6btR3qxQQwZqgMJA"
+                    target="_blank"
+                    className="inline-block text-sm px-4 py-2 leading-none border rounded text-blue-800 border-blue-800 hover:border-transparent hover:text-white hover:bg-blue-800 mt-4 lg:mt-0"
+                  >
+                    피드백 보내기
+                  </a>
+                </div>
               </li>
             </ul>
           </div>
@@ -33,4 +61,4 @@ export default function Header() {
       </nav>
     </header>
   );
-};
+}
--- frontend/src/Components/NoticeDetail.js
+++ frontend/src/Components/NoticeDetail.js
@@ -1,13 +1,53 @@
-import React from "react";
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-export default function NoticeDetail() {
+export default function NoticeDetail(props) {
   const { idx } = useParams();
+  const [noticeList, setNoticeList] = useState([
+    {
+      title: "스피드런 예시 영상",
+      content:
+        "<video className='first-video' width='480' height='255' controls><source src='https://s3.us-west-2.amazonaws.com/secure.notion-static.com/b8902982-f26c-400a-872f-9b617f54f6e3/%EC%B5%9C%EA%B3%A0%EA%B8%B0%EB%A1%9D.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221121%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221121T072831Z&X-Amz-Expires=86400&X-Amz-Signature=6cfaa0e1edbb6f4059c29536dbae3fb6bb21e0ba7a92c9ee7669bf117b8a40e2&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22%25EC%25B5%259C%25EA%25B3%25A0%25EA%25B8%25B0%25EB%25A1%259D.mp4%22&x-id=GetObject' type='video/mp4'/></video>",
+    },
+    {
+      time: "2022년 11월 21일 릴리즈 공지",
+      content:
+        "<p className='mb-6 text-base font-normal text-black-500 lg:text-base dark:text-black-400'>v 0.1.0</p><ul class='space-y-1 list-disc list-inside text-gray-500 dark:text-gray-400'><li>낚시 게임의 물고기 획득 확률 조정 및 UI의 개선이 이루어졌습니다.</li><li>Space Bar 버튼을 통해 영상을 Skip할 수 있는 기능이 추가되었습니다.</li><li>퀘스트 안내 UI가 추가되었습니다.</li><li>게임 UI 설명이 추가되었습니다.</li><li>스피드런 예시 영상</li></ul>",
+    },
+    { time: "2022년 11월 22일 릴리즈 공지", content: "" },
+  ]);
+
   return (
-    <div>
-      {idx === 1 ? <div>
-        공지1
-      </div> : <div>공지2</div>}
+    // <div>
+    //   {idx === 1 ? <div>
+    //     공지1
+    //   </div> : <div>공지2</div>}
+    // </div>
+
+    <div className="leaderboard-page">
+      <h1 className="mb-10 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-4xl">
+        공지사항
+      </h1>
+      <div className="leaderboard">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="py-5 px-6 text-base">
+                {noticeList[0].title}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <div
+                  dangerouslySetInnerHTML={{ __html: noticeList[1].content }}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
